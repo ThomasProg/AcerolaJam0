3872e9d077c4b5532e918356b1f6219a822633e2
Prog'z <progz.email@gmail.com>
2024-03-14 05:03:07 +0900

    player icon added to the minimap

--- Game/scripts/minimap/minimapScene.gd
+++ Game/scripts/minimap/minimapScene.gd
@@ -2,22 +2,32 @@ extends Node2D
 
 @export var roomsRoot:Node2D = null
 @export var connectionsRoot:Node2D = null
+@export var playerIcon:Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SaveManager.onRoomVisited.connect(func(room):
-		UpdateRoomsVisibility(roomsRoot)
-		UpdateConnectionsVisibility(connectionsRoot)
+		update()
 		)
 		
-	UpdateRoomsVisibility(roomsRoot)
-	UpdateConnectionsVisibility(connectionsRoot)
+	update()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func UpdateRoomVisibility(room:Node2D):
+func update():
+	UpdateRoomsVisibility(roomsRoot)
+	UpdateConnectionsVisibility(connectionsRoot)
+	updatePlayerIcon()
+
+func updatePlayerIcon():
+	if (SaveManager.currentRoom != null):
+		var room = roomsRoot.find_child(SaveManager.currentRoom.name) as Node2D
+		if (room != null):
+			playerIcon.global_position = room.global_position
+
+func updateRoomVisibility(room:Node2D):
 	if (SaveManager.visitedRooms.find(room.name) == -1):
 		room.visible = false
 	else:
@@ -26,9 +36,9 @@ func UpdateRoomVisibility(room:Node2D):
 func UpdateRoomsVisibility(roomsRoot:Node2D):
 	for category in roomsRoot.get_children():
 		for room in category.get_children():
-			UpdateRoomVisibility(room)
+			updateRoomVisibility(room)
 
-func UpdateConnectionVisibility(connection:RoomConnection):
+func updateConnectionVisibility(connection:RoomConnection):
 	if (connection.room1.visible or connection.room2.visible):
 		connection.visible = true
 	else:
@@ -36,4 +46,4 @@ func UpdateConnectionVisibility(connection:RoomConnection):
 
 func UpdateConnectionsVisibility(connectionsRoot:Node2D):
 	for connection in connectionsRoot.get_children():
-		UpdateConnectionVisibility(connection)
+		updateConnectionVisibility(connection)
